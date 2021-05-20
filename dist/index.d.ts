55df16a5369c53a4fd481899e21594eb83da07ae
Ediz Celik <mr.edizcelik@gmail.com>
2021-05-20 17:51:15 +0300

    build(2021-05-20/0.2.3): Build app

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,5 +1,5 @@
 export { getvalidatorAppID, getValidatorAppCreationTransaction, sendValidatorAppCreationTransaction, optIntoValidator, isOptedIntoValidator, closeOutOfValidator, } from './validator';
-export { PoolStatus, PoolInfo, MINIMUM_LIQUIDITY, getPoolInfo, createPool, getPoolReserves, } from './pool';
+export { PoolStatus, PoolInfo, PoolReserves, MINIMUM_LIQUIDITY, getPoolInfo, createPool, getPoolReserves, } from './pool';
 export { MintQuote, MintExecution, getMintLiquidityQuote, mintLiquidity, } from './mint';
 export { BurnQuote, BurnExecution, getBurnLiquidityQuote, burnLiquidity, } from './burn';
 export { SwapQuote, SwapExecution, getFixedInputSwapQuote, fixedInputSwap, getFixedOutputSwapQuote, fixedOutputSwap, } from './swap';
