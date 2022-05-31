d90da84a478f8269608910550507162374a7abfd
edizcelik <mr.edizcelik@gmail.com>
2022-05-31 16:35:30 +0300

    feat(staking): Extend commit txn util to support required asset programs

--- src/stake.ts
+++ src/stake.ts
@@ -1,17 +1,16 @@
-import {Algodv2, encodeUint64, makeApplicationNoOpTxnFromObject} from "algosdk";
+import {
+  Algodv2,
+  assignGroupID,
+  encodeUint64,
+  makeApplicationNoOpTxnFromObject,
+  SuggestedParams
+} from "algosdk";
 
 import {SignerTransaction, SupportedNetwork} from "./util/commonTypes";
 import {encodeString, joinByteArrays} from "./util/util";
 
-async function prepareCommitTransactions({
-  client,
-  stakingAppID,
-  initiatorAddr,
-  liquidityAssetID,
-  program,
-  amount
-}: {
-  client: Algodv2;
+interface CreateCommitTxnOptions {
+  suggestedParams: SuggestedParams;
   stakingAppID: number;
   initiatorAddr: string;
   liquidityAssetID: number;
@@ -20,28 +19,87 @@ async function prepareCommitTransactions({
     id: number;
   };
   amount: number | bigint;
-}): Promise<SignerTransaction[]> {
-  const suggestedParams = await client.getTransactionParams().do();
+}
 
+function createCommitTxnWithSuggestedParams({
+  suggestedParams,
+  stakingAppID,
+  initiatorAddr,
+  liquidityAssetID,
+  program,
+  amount
+}: CreateCommitTxnOptions) {
   const amountEncoded = encodeUint64(amount);
   const programIdEncoded = encodeUint64(program.id);
 
+  return makeApplicationNoOpTxnFromObject({
+    appIndex: stakingAppID,
+    from: initiatorAddr,
+    suggestedParams,
+    foreignAssets: [liquidityAssetID],
+    accounts: [program.accountAddress],
+    appArgs: [encodeString("commit"), amountEncoded],
+    note: joinByteArrays([
+      encodeString("tinymanStaking/v1:b"),
+      programIdEncoded,
+      encodeUint64(liquidityAssetID),
+      amountEncoded
+    ])
+  });
+}
+
+async function prepareCommitTransactions({
+  client,
+  stakingAppID,
+  program,
+  requiredAssetID,
+  liquidityAssetID,
+  amount,
+  initiatorAddr
+}: Omit<CreateCommitTxnOptions, "suggestedParams"> & {
+  client: Algodv2;
+  requiredAssetID?: number;
+}): Promise<SignerTransaction[]> {
+  const suggestedParams = await client.getTransactionParams().do();
+
+  const commitTxn = createCommitTxnWithSuggestedParams({
+    suggestedParams,
+    stakingAppID,
+    program,
+    liquidityAssetID,
+    initiatorAddr,
+    amount
+  });
+
+  let txnGroup = [commitTxn];
+
+  if (typeof requiredAssetID === "number") {
+    const logBalanceTxn = makeApplicationNoOpTxnFromObject({
+      appIndex: stakingAppID,
+      from: initiatorAddr,
+      suggestedParams,
+      foreignAssets: [requiredAssetID],
+      accounts: [program.accountAddress],
+      appArgs: [encodeString("log_balance")]
+    });
+
+    txnGroup = assignGroupID([commitTxn, logBalanceTxn]);
+
+    return [
+      {
+        txn: txnGroup[0],
+        signers: [initiatorAddr]
+      },
+      {
+        txn: txnGroup[1],
+        signers: [initiatorAddr]
+      }
+    ];
+  }
+
   return [
     {
-      txn: makeApplicationNoOpTxnFromObject({
-        appIndex: stakingAppID,
-        from: initiatorAddr,
-        suggestedParams,
-        foreignAssets: [liquidityAssetID],
-        accounts: [program.accountAddress],
-        appArgs: [encodeString("commit"), amountEncoded],
-        note: joinByteArrays([
-          encodeString("tinymanStaking/v1:b"),
-          programIdEncoded,
-          encodeUint64(liquidityAssetID),
-          amountEncoded
-        ])
-      }),
+      txn: txnGroup[0],
       signers: [initiatorAddr]
     }
   ];
