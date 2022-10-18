6049271395f6eb56e1da1686ddfb68f1553add51
Cem KIRAY <jamcry@hotmail.com>
2022-10-18 16:56:15 +0100

    fix(remove-liquidity): update v2 quote functions

--- src/burn/v2/index.ts
+++ src/burn/v2/index.ts
@@ -1,4 +1,4 @@
-import algosdk, {Algodv2, Transaction} from "algosdk";
+import algosdk, {Algodv2, ALGORAND_MIN_TX_FEE, Transaction} from "algosdk";
 
 import {tinymanContract_v2} from "../../contract/contract";
 import {
@@ -7,7 +7,7 @@ import {
   SupportedNetwork
 } from "../../util/commonTypes";
 import {PoolInfo, PoolReserves} from "../../util/pool/poolTypes";
-import {encodeString} from "../../util/util";
+import {sendAndWaitRawTransaction} from "../../util/util";
 import {
   V2RemoveLiquidityTxnIndices,
   V2_REMOVE_LIQUIDITY_APP_ARGUMENT,
@@ -43,35 +43,90 @@ export interface BurnQuote {
 export function getQuote({
   pool,
   reserves,
-  liquidityIn
+  /**
+   * previously `liquidityIn`
+   */
+  poolTokenAssetIn
 }: {
   pool: PoolInfo;
   reserves: PoolReserves;
-  liquidityIn: number | bigint;
+  poolTokenAssetIn: number | bigint;
 }): BurnQuote {
-  /**
-   * TODO: Will this work same with v1?
-   */
-  const liquidityIn_bigInt = BigInt(liquidityIn);
-
-  const asset1Out =
-    reserves.issuedLiquidity &&
-    (liquidityIn_bigInt * reserves.asset1) / reserves.issuedLiquidity;
-  const asset2Out =
-    reserves.issuedLiquidity &&
-    (liquidityIn_bigInt * reserves.asset2) / reserves.issuedLiquidity;
+  const poolTokenAssetIn_bigInt = BigInt(poolTokenAssetIn);
+  const {asset_1_output_amount, asset_2_output_amount} =
+    calculateRemoveLiquidityOutputAmounts(poolTokenAssetIn_bigInt, reserves);
 
   return {
     round: reserves.round,
     liquidityID: pool.liquidityTokenID!,
-    liquidityIn: liquidityIn_bigInt,
+    liquidityIn: poolTokenAssetIn_bigInt,
     asset1ID: pool.asset1ID,
-    asset1Out,
+    asset1Out: asset_1_output_amount,
     asset2ID: pool.asset2ID,
-    asset2Out
+    asset2Out: asset_2_output_amount
   };
 }
 
+export function getSingleAssetRemoveLiquidityQuote({
+  pool,
+  reserves,
+  /**
+   * previously `liquidityIn`
+   */
+  poolTokenAssetIn,
+  assetOutID
+}: {
+  pool: PoolInfo;
+  reserves: PoolReserves;
+  poolTokenAssetIn: number | bigint;
+  assetOutID: number;
+}): BurnQuote {
+  const poolTokenAssetIn_bigInt = BigInt(poolTokenAssetIn);
+  const {asset_1_output_amount, asset_2_output_amount} =
+    calculateRemoveLiquidityOutputAmounts(poolTokenAssetIn_bigInt, reserves);
+
+  console.log({assetOutID});
+  /**
+   * TODO: For the rest of this function, we need new swap utils.
+   * See: `fetch_single_asset_remove_liquidity_quote` in py sdk
+   */
+
+  throw new Error("Not completely implemented.");
+
+  return {
+    round: reserves.round,
+    liquidityID: pool.liquidityTokenID!,
+    liquidityIn: poolTokenAssetIn_bigInt,
+    asset1ID: pool.asset1ID,
+    asset1Out: asset_1_output_amount,
+    asset2ID: pool.asset2ID,
+    asset2Out: asset_2_output_amount
+  };
+}
+
+// TODO: what is this?
+const LOCKED_POOL_TOKENS = 1000;
+
+function calculateRemoveLiquidityOutputAmounts(
+  pool_token_asset_amount: number | bigint,
+  reserves: PoolReserves
+) {
+  let asset_1_output_amount: bigint, asset_2_output_amount: bigint;
+  const poolTokenAssetAmountBigInt = BigInt(pool_token_asset_amount);
+  const issuedPoolTokens = reserves.issuedLiquidity;
+
+  if (issuedPoolTokens > poolTokenAssetAmountBigInt + BigInt(LOCKED_POOL_TOKENS)) {
+    asset_1_output_amount =
+      (poolTokenAssetAmountBigInt * reserves.asset1) / issuedPoolTokens;
+    asset_2_output_amount =
+      (poolTokenAssetAmountBigInt * reserves.asset1) / issuedPoolTokens;
+  } else {
+    asset_1_output_amount = reserves.asset1;
+    asset_2_output_amount = reserves.asset2;
+  }
+
+  return {asset_1_output_amount, asset_2_output_amount};
+}
 /**
  * MULTIPLE ASSET OUT
  * py-sdk reference: prepare_remove_liquidity_transactions
@@ -80,14 +135,17 @@ export function getQuote({
 async function generateRemoveLiquidityTxns({
   client,
   pool,
-  liquidityIn,
+  poolTokenAssetAmount,
   initiatorAddr,
   minAsset1Amount,
   minAsset2Amount
 }: {
   client: Algodv2;
   pool: PoolInfo;
-  liquidityIn: number | bigint;
+  /**
+   * previously: `liquidityIn`
+   */
+  poolTokenAssetAmount: number | bigint;
   initiatorAddr: string;
   minAsset1Amount: number | bigint;
   minAsset2Amount: number | bigint;
@@ -98,14 +156,17 @@ async function generateRemoveLiquidityTxns({
 
   const suggestedParams = await client.getTransactionParams().do();
   const poolAddress = pool.account.address();
+  const poolTokenAssetId = pool.liquidityTokenID;
+
+  if (!poolTokenAssetId) {
+    throw new Error("Pool token asset ID is missing");
+  }
 
   const assetTransferTxn = algosdk.makeAssetTransferTxnWithSuggestedParamsFromObject({
     from: initiatorAddr,
     to: poolAddress,
-    // TODO: what if liquidityTokenID is falsy?
-    assetIndex: pool.liquidityTokenID!,
-    // TODO: is amt value true? Is it same with `pool_token_asset_amount` in py sdk?
-    amount: liquidityIn,
+    assetIndex: poolTokenAssetId,
+    amount: poolTokenAssetAmount,
     suggestedParams
   });
 
@@ -114,17 +175,15 @@ async function generateRemoveLiquidityTxns({
     appIndex: pool.validatorAppID,
     appArgs: [
       V2_REMOVE_LIQUIDITY_APP_ARGUMENT,
-
-      // TODO: Probably these encoding won't work for int
-      encodeString(String(minAsset1Amount)),
-      encodeString(String(minAsset2Amount))
+      algosdk.encodeUint64(minAsset1Amount),
+      algosdk.encodeUint64(minAsset2Amount)
     ],
     accounts: [poolAddress],
     foreignAssets: [pool.asset1ID, pool.asset2ID],
     suggestedParams: {
       ...suggestedParams,
-      // Add +1 for outer txn cost
-      fee: (V2_REMOVE_LIQUIDITY_APP_CALL_INNER_TXN_COUNT + 1) * suggestedParams.fee
+      // Add + 1 for outer txn cost
+      fee: (V2_REMOVE_LIQUIDITY_APP_CALL_INNER_TXN_COUNT + 1) * ALGORAND_MIN_TX_FEE
     }
   });
 
@@ -140,7 +199,10 @@ async function generateRemoveLiquidityTxns({
       txn: txGroup[V2RemoveLiquidityTxnIndices.ASSET_TRANSFER_TXN],
       signers: [initiatorAddr]
     },
-    {txn: txGroup[V2RemoveLiquidityTxnIndices.APP_CALL_TXN], signers: [poolAddress]}
+    {
+      txn: txGroup[V2RemoveLiquidityTxnIndices.APP_CALL_TXN],
+      signers: [poolAddress]
+    }
   ];
 }
 
@@ -152,21 +214,29 @@ async function generateRemoveLiquidityTxns({
 async function generateSingleAssetOutRemoveLiquidityTxns({
   client,
   pool,
-  outputAssetId,
-  liquidityIn,
   initiatorAddr,
+  poolTokenAssetAmount,
+  outputAssetId,
   minOutputAssetAmount
 }: {
   client: Algodv2;
   pool: PoolInfo;
   outputAssetId: number;
-  liquidityIn: number | bigint;
+  /**
+   * previously: `liquidityIn`
+   */
+  poolTokenAssetAmount: number | bigint;
   initiatorAddr: string;
   minOutputAssetAmount: number | bigint;
 }): Promise<SignerTransaction[]> {
   const suggestedParams = await client.getTransactionParams().do();
   const {asset1ID, asset2ID} = pool;
   const poolAddress = pool.account.address();
+  const poolTokenAssetId = pool.liquidityTokenID;
+
+  if (!poolTokenAssetId) {
+    throw new Error("Pool token asset ID is missing");
+  }
 
   let minAsset1Amount = 0 as number | bigint;
   let minAsset2Amount = 0 as number | bigint;
@@ -184,10 +254,9 @@ async function generateSingleAssetOutRemoveLiquidityTxns({
   const assetTransferTxn = algosdk.makeAssetTransferTxnWithSuggestedParamsFromObject({
     from: initiatorAddr,
     to: poolAddress,
-    // TODO: what if liquidityTokenID is falsy?
-    assetIndex: pool.liquidityTokenID!,
+    assetIndex: poolTokenAssetId,
     // TODO: is amt value true?
-    amount: liquidityIn,
+    amount: poolTokenAssetAmount,
     suggestedParams
   });
 
@@ -196,15 +265,14 @@ async function generateSingleAssetOutRemoveLiquidityTxns({
     appIndex: pool.validatorAppID,
     appArgs: [
       V2_REMOVE_LIQUIDITY_APP_ARGUMENT,
-      // TODO: Probably these encoding won't work for int
-      encodeString(String(minAsset1Amount)),
-      encodeString(String(minAsset2Amount))
+      algosdk.encodeUint64(minAsset1Amount),
+      algosdk.encodeUint64(minAsset2Amount)
     ],
     accounts: [poolAddress],
     foreignAssets: [pool.asset1ID, pool.asset2ID],
     suggestedParams: {
       ...suggestedParams,
-      // Add +1 for outer txn cost
+      // Add + 1 for outer txn cost
       fee: (V2_REMOVE_LIQUIDITY_APP_CALL_INNER_TXN_COUNT + 1) * suggestedParams.fee
     }
   });
@@ -221,7 +289,10 @@ async function generateSingleAssetOutRemoveLiquidityTxns({
       txn: txGroup[V2RemoveLiquidityTxnIndices.ASSET_TRANSFER_TXN],
       signers: [initiatorAddr]
     },
-    {txn: txGroup[V2RemoveLiquidityTxnIndices.APP_CALL_TXN], signers: [poolAddress]}
+    {
+      txn: txGroup[V2RemoveLiquidityTxnIndices.APP_CALL_TXN],
+      signers: [poolAddress]
+    }
   ];
 }
 
@@ -255,9 +326,30 @@ async function signTxns({
   return signedTxns;
 }
 
-function execute(_args: any) {
-  // TODO
-  throw new Error("Method not implemented.");
+async function execute({
+  client,
+  // pool,
+  // txGroup,
+  signedTxns
+}: // initiatorAddr
+{
+  client: any;
+  // pool: PoolInfo;
+  // txGroup: SignerTransaction[];
+  signedTxns: Uint8Array[];
+  // initiatorAddr: string;
+}) {
+  const [data] = await sendAndWaitRawTransaction(client, [signedTxns]);
+
+  console.log({
+    data
+  });
+
+  return {
+    // round: confirmedRound,
+    // txnID,
+    data
+  };
 }
 
 export const BurnV2 = {
