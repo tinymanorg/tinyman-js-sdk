24c29824b027218dd8f156d693f48d9128e2f4fe
Cem KIRAY <jamcry@hotmail.com>
2022-10-24 19:51:11 +0100

    chore(burn): removeo old burn folder

--- src/burn/index.ts
+++
@@ -1,8 +0,0 @@
-import {CONTRACT_VERSION} from "../contract/constants";
-import {BurnV1_1} from "./v1_1";
-import {BurnV2} from "./v2";
-
-export const Burn = {
-  [CONTRACT_VERSION.V1_1]: BurnV1_1,
-  [CONTRACT_VERSION.V2]: BurnV2
-};
