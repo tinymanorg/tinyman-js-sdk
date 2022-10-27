ca5142a4bfd48caf38a9581badd252751df39de1
Cem KIRAY <jamcry@hotmail.com>
2022-10-27 11:06:20 +0200

    chore(swap): export `V2SwapExecution`

--- src/swap/v2/index.ts
+++ src/swap/v2/index.ts
@@ -116,7 +116,7 @@ function getSwapAppCallFeeAmount(swapType: SwapType) {
   return totalTxnCount * ALGORAND_MIN_TX_FEE;
 }
 
-interface V2SwapExecution {
+export interface V2SwapExecution {
   assetIn: {assetID: number; amount: number | bigint};
   assetOut: {assetID: number; amount: number | bigint};
   pool: V2PoolInfo;
