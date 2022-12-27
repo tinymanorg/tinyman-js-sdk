58e76ec921084063643e9eb7b2c6d494b9397584
Cem KIRAY <jamcry@hotmail.com>
2022-12-27 18:08:54 +0100

    chore: remove unused approval and clear program data

--- src/contract/base/contract.ts
+++ src/contract/base/contract.ts
@@ -1,5 +1,4 @@
 import {LogicSigAccount} from "algosdk";
-import {toByteArray} from "base64-js";
 
 import {SupportedNetwork} from "../../util/commonTypes";
 import {V1_1ValidatorApp} from "../v1_1/types";
@@ -15,15 +14,9 @@ export interface ValidatorAppSchema {
 export abstract class BaseTinymanContract<
   ValidatorApp extends V1_1ValidatorApp | V2ValidatorApp
 > {
-  validatorApprovalContract: Uint8Array;
-  validatorClearStateContract: Uint8Array;
-
   schema: ValidatorAppSchema;
 
   constructor(validatorApp: ValidatorApp) {
-    this.validatorApprovalContract = toByteArray(validatorApp.approval_program.bytecode);
-    this.validatorClearStateContract = toByteArray(validatorApp.clear_program.bytecode);
-
     this.schema = {
       numLocalInts: validatorApp.local_state_schema.num_uints,
       numLocalByteSlices: validatorApp.local_state_schema.num_byte_slices,
