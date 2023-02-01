c5926d6dd57fe7a82474c537a1a5de81c34e65a9
Cem KIRAY <jamcry@hotmail.com>
2023-02-01 11:56:40 +0100

    chore: improvements

--- examples/src/util/client.ts
+++ examples/src/util/client.ts
@@ -1,10 +1,10 @@
 import algosdk from "algosdk";
 
-const ALGOD_TOKEN: string | undefined = undefined;
-
-if (!ALGOD_TOKEN) {
-  throw new Error(`⚠️ ALGOD_TOKEN is not set. Please update it inside "src/util/client.ts".
-For algosdk reference, see: https://algorand.github.io/js-algorand-sdk/#quick-start`);
-}
-
-export const algodClient = new algosdk.Algodv2(ALGOD_TOKEN);
+console.log(
+  "ℹ️  The examples uses the testnet AlgoExplorer API for testing purposes. You can change it in `client.ts` file.\n"
+);
+export const algodClient = new algosdk.Algodv2(
+  "",
+  "https://node.testnet.algoexplorerapi.io/",
+  ""
+);
