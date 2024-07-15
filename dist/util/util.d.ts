279e50809a93bbc7d7f982db96883f99bcdb536f
gulcinuras <gulcinuras@sabanciuniv.edu>
2024-07-15 11:13:33 +0200

    fix(encode-integer): Add support for larger numbers

--- dist/util/util.d.ts
+++ dist/util/util.d.ts
@@ -51,7 +51,7 @@ export declare function sendAndWaitRawTransaction(client: Algodv2, signedTxnGrou
 }[]>;
 export declare function sumUpTxnFees(txns: SignerTransaction[]): number;
 export declare function getTxnGroupID(txns: SignerTransaction[]): string;
-export declare function encodeInteger(number: any): number[];
+export declare function encodeInteger(number: bigint): number[];
 /**
  * Converts a text into bytes
  */
