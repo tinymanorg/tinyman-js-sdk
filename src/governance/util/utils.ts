d8bb2612fbe193568dfd26efb94181d13b447a5e
gulcinuras <gulcinuras@sabanciuniv.edu>
2024-08-07 11:16:27 +0200

    fix(utils): Fix int to byte conversion for large values

--- src/governance/util/utils.ts
+++ src/governance/util/utils.ts
@@ -1,11 +1,11 @@
 /* eslint-disable no-bitwise */
 export function intToBytes(num: number, length = 8): Uint8Array {
   const byteArray = new Uint8Array(length);
-  let newNum = num;
+  let newNum = BigInt(num);
 
   for (let i = length - 1; i >= 0; i--) {
-    byteArray[i] = newNum & 0xff;
-    newNum >>= 8;
+    byteArray[i] = Number(newNum & BigInt(0xff));
+    newNum >>= BigInt(8);
   }
 
   return byteArray;
@@ -16,16 +16,16 @@ export function areBuffersEqual(buf1: Uint8Array, buf2: Uint8Array) {
 }
 
 export function bytesToInt(buffer: Uint8Array): number {
-  let num = 0;
+  let num = 0n;
 
   for (let i = 0; i < buffer.byteLength; i++) {
     const byte = buffer[i];
 
-    num *= 0x100;
-    num += byte;
+    num *= BigInt(0x100);
+    num += BigInt(byte);
   }
 
-  return num;
+  return Number(num);
 }
 
 export function sum(values: number[]): number {
